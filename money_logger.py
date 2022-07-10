6051e65c9f9edc167b6d734e3a0b145aa4c35510
C0okiesnCream <69062323+C0okiesnCream@users.noreply.github.com>
2022-07-09 21:25:32 -0600

    adding month variables
    I was gonna do something else with this but I forgot what I was planning.

--- money_logger.py
+++ money_logger.py
@@ -2,3 +2,17 @@
 import tkinter
 import pickle
 import decimal
+
+#variables
+month0 = 0 #this variable will be used to see the file of the current month
+month1 = 0 #variables month(1 - 11) will be used to see the last
+month2 = 0 #12 months(including the current one(month0)), if possible
+month3 = 0 #it wont show every day of the month, only the gross
+month4 = 0
+month5 = 0
+month6 = 0
+month7 = 0
+month8 = 0
+month9 = 0
+month10 = 0
+month11 = 0
