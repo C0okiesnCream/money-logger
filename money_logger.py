6bda38e2c662779a7bc974c6ab0e10402a64d161
C0okiesnCream <69062323+C0okiesnCream@users.noreply.github.com>
2022-07-13 16:31:13 -0600

    getting the value class set up
    not completely

--- money_logger.py
+++ money_logger.py
@@ -1,18 +1,24 @@
 #imports
-import tkinter
-import pickle
-import decimal
+import decimal #seeing as how the software will be dealing with money values, gonna need decimal
+import pickle #python based serialization module, will be used for serializing objects mainly
+import tkinter as tkntr #wanna have a decent graphical interface
+import sqlite3 as SQL3 #think a database will be a bit nicer than a txt file to store values
 
 #variables
-month0 = 0 #this variable will be used to see the file of the current month
-month1 = 0 #variables month(1 - 11) will be used to see the last
-month2 = 0 #12 months(including the current one(month0)), if possible
-month3 = 0 #it wont show every day of the month, only the gross
-month4 = 0
-month5 = 0
-month6 = 0
-month7 = 0
-month8 = 0
-month9 = 0
-month10 = 0
-month11 = 0
+
+#classes
+class value: #this is going to be the main class for this project
+ def __init__(self, month, date, year, money):
+  self.month = month
+  self.date = date
+  self.year = year
+  self.money = money
+
+ def __lt__(self, other): #not completely sure if this will work but dont have enough built to test it yet so we'll see ¯\_(ツ)_/¯
+  return self.year < other.year
+  return self.month < other.month
+  return self.date < other.date
+ 
+#functions
+def loadGui(): #used to load the GUI via tkinter
+ pass
